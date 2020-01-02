482e3f9b7af189b862c04798c6ff9ff9bc033e76
Sam <amazing_sam@outlook.com>
2020-01-03 00:41:15 +0800

    优化babel配置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button } from "ant-design-vue";
-// import Antd from "ant-design-vue";
-// import 'ant-design-vue/dist/antd.less'
 
+// 加载样式文件
+// import 'ant-design-vue/dist/antd.less'
+// 全部组件都注册, 这会导致加载网页时的JS文件异常庞大.
+// import Antd from "ant-design-vue";
+// Vue.use(Antd);
+
+import { Button } from "ant-design-vue";
 Vue.use(Button);
+
 Vue.config.productionTip = false;
 
 new Vue({
